3f952ad9ee915a112081d8e7d96c3d9a1a63f312
AmbrizAlberto <albertpoambez@gmail.com>
2025-01-26 00:29:13 -0600

    feat: about page done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,166 @@
+"use client";
+
+import aboutstyles from "../../../public/css/about.css"
+
+export default function About() {
+  return (
+    <div>
+        <header className="hero">
+            <div className="hero-content">
+                <h1>Descubra las cualidades únicas que distinguen a Gimnasio St. John´s</h1>
+                <p>Descubra cómo nuestro compromiso con la excelencia, nuestros programas innovadores y nuestra comunidad apasionada hacen de FitLife Studio el destino definitivo para su viaje de fitness.</p>
+            </div>
+        </header>
+
+        <main>
+            <section className="part2">
+            <div className="about-content">
+                <h2>Acerca de Gimnasio St. John´s</h2>
+                <p>En Gimnasio St. John´s, creemos que el fitness es más que una rutina: es una forma de vida. Fundado con la misión de inspirar y empoderar a las personas en sus recorridos de fitness, ofrecemos una amplia gama de servicios diseñados para satisfacer las necesidades únicas de cada miembro. Nuestras instalaciones de última generación, entrenadores expertos y una comunidad vibrante crean un entorno en el que todos pueden prosperar.</p>
+                <h3>Nuestra misión</h3>
+                <p>Nuestra misión es brindar soluciones de acondicionamiento físico de primer nivel que ayuden a nuestros miembros a lograr sus objetivos personales de salud y bienestar. Nos dedicamos a fomentar un ambiente motivador y de apoyo, donde cada individuo se sienta valorado e inspirado para superar sus límites.</p>
+            </div>
+            <div className="about-image">
+                <img src="https://i5.walmartimages.com/asr/37c9016a-7df4-4cd0-a7d6-5a306314b2a0.31224581ceaed791c4dac8e389b05e29.jpeg" alt="Persona levantando pesas"/>
+            </div>
+            </section>
+
+            <section className="part3">
+                <div className="gymteamdesc">
+                    <h1>Conoce a nuestros entrenadores expertos</h1>
+                    <p>Conozca a los profesionales apasionados y capacitados dedicados a ayudarlo a alcanzar sus objetivos de acondicionamiento físico.</p>
+                </div>
+                <div className="card-container">
+                    <div className="card">
+                        <img src="https://via.placeholder.com/150" alt="Entrenador 2" className="card-image"/>
+                        <h3 className="card-title">Nombre 2</h3>
+                        <p className="card-subtitle">Maestro Fitness Instructor</p>
+                        <div className="card-icons">
+                            <a href="#" className="icon">LinkedIn</a>
+                            <a href="#" className="icon">✖️</a>
+                            <a href="#" className="icon">🌐</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://via.placeholder.com/150" alt="Entrenador 2" className="card-image"/>
+                        <h3 className="card-title">Nombre 2</h3>
+                        <p className="card-subtitle">Maestro Fitness Instructor</p>
+                        <div className="card-icons">
+                            <a href="#" className="icon">LinkedIn</a>
+                            <a href="#" className="icon">✖️</a>
+                            <a href="#" className="icon">🌐</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://via.placeholder.com/150" alt="Entrenador 2" className="card-image"/>
+                        <h3 className="card-title">Nombre 2</h3>
+                        <p className="card-subtitle">Maestro Fitness Instructor</p>
+                        <div className="card-icons">
+                            <a href="#" className="icon">LinkedIn</a>
+                            <a href="#" className="icon">✖️</a>
+                            <a href="#" className="icon">🌐</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://via.placeholder.com/150" alt="Entrenador 2" className="card-image"/>
+                        <h3 className="card-title">Nombre 2</h3>
+                        <p className="card-subtitle">Maestro Fitness Instructor</p>
+                        <div className="card-icons">
+                            <a href="#" className="icon">LinkedIn</a>
+                            <a href="#" className="icon">✖️</a>
+                            <a href="#" className="icon">🌐</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://via.placeholder.com/150" alt="Entrenador 2" className="card-image"/>
+                        <h3 className="card-title">Nombre 2</h3>
+                        <p className="card-subtitle">Maestro Fitness Instructor</p>
+                        <div className="card-icons">
+                            <a href="#" className="icon">LinkedIn</a>
+                            <a href="#" className="icon">✖️</a>
+                            <a href="#" className="icon">🌐</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://via.placeholder.com/150" alt="Entrenador 2" className="card-image"/>
+                        <h3 className="card-title">Nombre 2</h3>
+                        <p className="card-subtitle">Maestro Fitness Instructor</p>
+                        <div className="card-icons">
+                            <a href="#" className="icon">LinkedIn</a>
+                            <a href="#" className="icon">✖️</a>
+                            <a href="#" className="icon">🌐</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://via.placeholder.com/150" alt="Entrenador 2" className="card-image"/>
+                        <h3 className="card-title">Nombre 2</h3>
+                        <p className="card-subtitle">Maestro Fitness Instructor</p>
+                        <div className="card-icons">
+                            <a href="#" className="icon">LinkedIn</a>
+                            <a href="#" className="icon">✖️</a>
+                            <a href="#" className="icon">🌐</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://via.placeholder.com/150" alt="Entrenador 2" className="card-image"/>
+                        <h3 className="card-title">Nombre 2</h3>
+                        <p className="card-subtitle">Maestro Fitness Instructor</p>
+                        <div className="card-icons">
+                            <a href="#" className="icon">LinkedIn</a>
+                            <a href="#" className="icon">✖️</a>
+                            <a href="#" className="icon">🌐</a>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <img src="https://via.placeholder.com/150" alt="Entrenador 2" className="card-image"/>
+                        <h3 className="card-title">Nombre 2</h3>
+                        <p className="card-subtitle">Maestro Fitness Instructor</p>
+                        <div className="card-icons">
+                            <a href="#" className="icon">LinkedIn</a>
+                            <a href="#" className="icon">✖️</a>
+                            <a href="#" className="icon">🌐</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="part4">
+                <div className="location">
+                    <h1>Visita Gimnasio St. John´s</h1>
+                    <p>Ubicados convenientemente para satisfacer sus necesidades de acondicionamiento físico. ¡Encuéntrenos y comience su viaje hoy mismo!</p>
+                </div>
+            </section>
+
+            <div className="container">
+                <div className="info-card">
+                    <h2 className="title">DIRECCIÓN</h2>
+                    <p className="subtitle">Gimnasio St. John´s | Manzanillo</p>
+                    <p className="address">
+                    Av. Paseo de Las Gaviotas s/n, Valle de las Garzas barrio 5, <br />
+                    28219 Manzanillo, Col.
+                    </p>
+                    <p className="phone">+52 314 334 7544</p>
+                    <a
+                    href="https://maps.app.goo.gl/SEYxYoZsgfNXBJbv5"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="map-link"
+                    >
+                    Ver mapa
+                    </a>
+                </div>
+                <div className="map">
+                    <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.050212947993!2d-104.32058992549923!3d19.105452982105174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8424d6711fb0c985%3A0xcbb57bc1496bebbe!2sGimnasio%20St.%20John%C2%B4s!5e0!3m2!1ses!2smx!4v1737868974631!5m2!1ses!2smx"
+                    width="100%"
+                    height="100%"
+                    style={{ border: 0 }}
+                    allowFullScreen=""
+                    loading="lazy"
+                    ></iframe>
+                </div>
+                </div>
+            
+        </main>
+    </div>
+    );
+}
